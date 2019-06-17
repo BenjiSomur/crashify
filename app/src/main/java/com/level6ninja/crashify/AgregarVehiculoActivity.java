9642fa7c5523e140cc5e6b813de078fead52d36b
Manolo Perez <manolope338@gmail.com>
2019-06-17 02:16:30 -0500

    Vehículos

--- app/src/main/java/com/level6ninja/crashify/AgregarVehiculoActivity.java
+++ app/src/main/java/com/level6ninja/crashify/AgregarVehiculoActivity.java
@@ -2,6 +2,7 @@ package com.level6ninja.crashify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AgregarVehiculoActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class AgregarVehiculoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_vehiculo);
+    }
+
+    public void onClickAgregarVehiculo(View views) {
+
     }
 }
